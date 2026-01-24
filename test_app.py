49794a754bf69d1f8548f9fc8808dc5bd9a14548
Kashish-0931 <145748121+Kashish-0931@users.noreply.github.com>
2026-01-24 21:30:52 +0530

    Update and rename tests/test_ci_trigger.py to test_app.py

---
+++ test_app.py
@@ -0,0 +1,23 @@
+# test_app.py
+import pytest
+import app
+
+def test_recommend_output_type():
+    """Check that recommend() returns a list"""
+    movie = app.movies.iloc[0].title
+    result = app.recommend(movie)
+    assert isinstance(result, list)
+
+def test_recommend_output_length():
+    """Check that recommend() returns 5 movies"""
+    movie = app.movies.iloc[0].title
+    result = app.recommend(movie)
+    assert len(result) == 5
+
+def test_movies_pickle_loads():
+    """Check that pickle files load correctly"""
+    import pickle
+    movies = pickle.load(open("movies_dict.pkl", "rb"))
+    similarity = pickle.load(open("similarity.pkl", "rb"))
+    assert len(movies) > 0
+    assert len(similarity) > 0
